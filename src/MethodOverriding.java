7715ab48341d6c1ce0a27301dc2f2b5da1335984
dragdab <139127963+dragdab@users.noreply.github.com>
2026-02-13 09:35:43 +0530

    Implement method overriding in Engineer class

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -1,26 +1,27 @@
 class Employee {
+    
     void display() {
         System.out.println("Name of class is Employee");
     }
 }
 
 class Engineer extends Employee {
-    public class MethodOverriding {
    
-        Engineer eng = new Engineer();
-        
-        eng.display();
-    }
     @Override
     void display() {
+       
         System.out.println("Name of class is Engineer");
+        
         
         super.display();
     }
 
-
-
+    public static void main(String[] args) {
+        
+        Engineer eng = new Engineer();
+        
+        
+        eng.display();
+    }
 }
-
-
        
