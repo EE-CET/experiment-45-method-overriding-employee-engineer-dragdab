0ebc7155dd858e2fc37a5a6cef9036ee2f044a82
dragdab <139127963+dragdab@users.noreply.github.com>
2026-02-13 09:29:05 +0530

    Override display method in Engineer class

--- src/MethodOverriding.java
+++ src/MethodOverriding.java
@@ -5,22 +5,19 @@ class Employee {
 }
 
 class Engineer extends Employee {
-    // Overriding the display method
+    
     @Override
     void display() {
         System.out.println("Name of class is Engineer");
         
-        // Calling the base class display method using super
         super.display();
     }
 
 
 public class MethodOverriding {
    
-         // Creating an Engineer object
         Engineer eng = new Engineer();
         
-        // Calling display() on the Engineer object
         eng.display();
     }
 }
